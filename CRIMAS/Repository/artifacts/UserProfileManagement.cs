dedbb42fd8d8b9d9e4d0e49efe456adef14dc573
Daniel Adigun <daniel.adigun@digitalforte.ng>
2016-02-18 11:39:07 +0100

    Skeleton of UserProfileManagement

---
+++ CRIMAS/Repository/artifacts/UserProfileManagement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRIMAS.Repository.artifacts
+{
+    public class UserProfileManagement
+    {
+        public void changePassword(string old, string newPassword)
+        {
+            //TODO: change user password
+        }
+
+        public void uploadProfilePicture(byte[] image)
+        {
+            //TODO: upload image
+        }
+    }
+}
