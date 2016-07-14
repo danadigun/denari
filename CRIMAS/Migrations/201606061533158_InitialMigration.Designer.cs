c1cffa2a18211567b9c0d4dcc855daafae54d775
Daniel Adigun <daniel.adigun@digitalforte.ng>
2016-07-14 23:05:13 +0100

    Created comming soon branch

--- CRIMAS/Migrations/201606061533158_InitialMigration.Designer.cs
+++ CRIMAS/Migrations/201606061533158_InitialMigration.Designer.cs
@@ -7,13 +7,13 @@ namespace CRIMAS.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class iniitlaanldasf : IMigrationMetadata
+    public sealed partial class InitialMigration : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(iniitlaanldasf));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(InitialMigration));
         
         string IMigrationMetadata.Id
         {
-            get { return "201512172242098_iniitlaanldasf"; }
+            get { return "201606061533158_InitialMigration"; }
         }
         
         string IMigrationMetadata.Source
--- CRIMAS/Migrations/Configuration.cs
+++ CRIMAS/Migrations/Configuration.cs
@@ -1,18 +1,15 @@
 namespace CRIMAS.Migrations
 {
-    using CRIMAS.Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
-    using System.Web.Security;
-    using WebMatrix.WebData;
 
     internal sealed class Configuration : DbMigrationsConfiguration<CRIMAS.Models.CrimasDb>
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationsEnabled = false;
         }
 
         protected override void Seed(CRIMAS.Models.CrimasDb context)
@@ -29,54 +26,6 @@ namespace CRIMAS.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
-            SeedCustomer();
-
-            SeedMembership();
-        }
-
-        private void SeedCustomer()
-        {
-            var db = new CRIMAS.Models.CrimasDb();
-
-            db.Customers.AddOrUpdate(
-                 p => p.Name,
-                new CRIMAS.Models.Customer {
-                    AccountNo =new Random().Next().ToString(),
-                    Name="Olumide Jegede",
-                    NextOfkin="Olumide Jegede",
-                    StateOfOrigin="jagsdgad",
-                    OfficeAddress="dhgfjkgfabfha",
-                    ResidentialAddress="dasgdfasbgfhjaf",
-                    DateCreated=DateTime.Now.ToLongDateString(),
-                    Email = "olumide@yahoo.com",
-                    phone = "070387273812"
-                    //VillageClan="fgfjk"
-                    }
-                );
-            db.SaveChanges();
-        }
-        private void SeedMembership()
-        {
-            WebSecurity.InitializeDatabaseConnection("DefaultConnection", "UserProfile", "UserId", "UserName", autoCreateTables: true);
-
-            var roles = (SimpleRoleProvider)Roles.Provider;
-            var membership = (SimpleMembershipProvider)Membership.Provider;
-            var _db = new CrimasDb();
-
-            //Create Admin role
-            if (!roles.RoleExists("Admin"))
-            {
-                roles.CreateRole("Admin");
-            }
-            if (membership.GetUser("admin@digitalforte.ng", false) == null)
-            {
-                membership.CreateUserAndAccount("admin@digitalforte.ng", "pass119");
-            }
-            if (!roles.GetRolesForUser("admin@digitalforte.ng").Contains("Admin"))
-            {
-                roles.AddUsersToRoles(new[] { "admin@digitalforte.ng" }, new[] { "Admin" });
-            }
-
         }
     }
 }
