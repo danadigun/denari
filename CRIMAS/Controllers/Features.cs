20a33f9bb482d2cc60ab61d373b89891cc0de16f
Daniel Adigun <daniel.adigun@digitalforte.ng>
2016-08-27 02:20:41 +0100

    designed new features and benefits page

---
+++ CRIMAS/Controllers/Features.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRIMAS.Controllers
+{
+    public class FeaturesController : Controller
+    {
+        public ActionResult index()
+        {
+            return View();
+        }
+    }
+}
