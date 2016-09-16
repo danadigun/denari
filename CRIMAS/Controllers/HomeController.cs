90ecf2f0d0733044c7b79bc0e784595871aafc94
Daniel Adigun <daniel.adigun@digitalforte.ng>
2016-09-16 15:18:39 +0100

    Added payment options

--- CRIMAS/Controllers/HomeController.cs
+++ CRIMAS/Controllers/HomeController.cs
@@ -64,6 +64,83 @@ namespace CRIMAS.Controllers
             return RedirectToAction("requestConfirm");
         }
 
+        [HttpPost]
+        public ActionResult orderRequest(string plan)
+        {
+            string plan_name = null;
+            string members = null;
+            decimal amount = Convert.ToInt32(plan) * 100;
+
+            switch (plan)
+            {
+                case "25,250":
+                    {
+                         plan_name = "Bronze - 3Months";
+                         members = "0-100";
+                         break;
+                    }
+                case "23,900":
+                    {
+                        plan_name = "Bronze - 12Months";
+                        members = "0-100";
+                        break;
+                    }
+                case "20,200":
+                    {
+                        plan_name = "Bronze - 36Months";
+                        members = "0-100";
+                        break;
+                    }
+                case "32,250":
+                    {
+                         plan_name = "Silver - 3Months";
+                        members = "101-300";
+                        break;
+                    }
+                case "30,600":
+                    {
+                        plan_name = "Silver - 12Months";
+                        members = "101-300";
+                        break;
+                    }
+                case "25,800":
+                    {
+                         plan_name = "Silver - 36Months";
+                        members = "101-300";
+                        break;
+                    }
+                case "42,250":
+                    {
+                        plan_name = "Gold - 3 Months";
+                        members = "Above 300";
+                        break;
+                    }
+                case "40,100":
+                    {
+                        plan_name = "Gold - 12 Months";
+                        members = "Above 300";
+                        break;
+                    }
+                case "36,200":
+                    {
+                        plan_name = "Gold - 36 Months";
+                        members = "Above 300";
+                        break;
+                    }
+                default:
+                    {
+                        plan_name = null;
+                        break;
+                    }
+            }
+
+            ViewBag.plan_name = plan_name;
+            ViewBag.members = members;
+            ViewBag.amount = amount;
+
+            return View();
+        }
+
         public ActionResult requestConfirm()
         {
             return View();
