1657e178cdee99753483ba6d7d9bf68f4d16df4f
Daniel Adigun <daniel.adigun@digitalforte.ng>
2016-10-12 03:03:49 +0100

    Finalized amortization redesign process

--- CRIMAS/Controllers/LoanTransactionController.cs
+++ CRIMAS/Controllers/LoanTransactionController.cs
@@ -39,8 +39,9 @@ namespace CRIMAS.Controllers
         //
         // GET: /LoanTransaction/Create
 
-        public ActionResult Create()
+        public ActionResult Create(string accountNo = "")
         {
+            ViewBag.AccountNo = accountNo;
             return View();
         }
 
@@ -50,61 +51,55 @@ namespace CRIMAS.Controllers
         public ActionResult CheckActiveLoan(string accountNo)
         {
             var activeloans = db.Loans.Where(x => x.AccountNo == accountNo).Where(x => x.LoanStatus == "active").ToList();
+            var Customer_Id = db.Customers.FirstOrDefault(x => x.AccountNo == accountNo).CustomerId;
 
-            return Json(new { Loans = activeloans });
+            return Json(new {CustomerId = Customer_Id,  Loans = activeloans });
         }
         
         [HttpPost]
         public ActionResult add(int loanId, decimal amount)
         {
             var loan = db.Loans.SingleOrDefault(x=>x.Id==loanId);
+        
             if(loan != null)
             {
-                db.LoanTransactions.Add(new LoanTransaction
+                var total_cr = db.LoanTransactions.Where(x => x.Loan.Id == loanId).Select(x => x.Cr).Sum();
+                var total_dr = db.LoanTransactions.Where(x => x.Loan.Id == loanId).Select(x => x.Dr).Sum();
+
+                if(total_cr > total_dr)
                 {
-                    AccountNo = loan.AccountNo,
-                    Cr = 0,
-                    createdby = User.Identity.Name,
-                    DateCreated = DateTime.Now.ToLongDateString(),
-                    Dr = amount,
-                    Loan = loan,
-                    Narration = "Loan Repayment"
-                });
-                db.SaveChanges();
+                    db.LoanTransactions.Add(new LoanTransaction
+                    {
+                        AccountNo = loan.AccountNo,
+                        Cr = 0,
+                        createdby = User.Identity.Name,
+                        DateCreated = DateTime.Now.ToLongDateString(),
+                        Dr = amount,
+                        Loan = loan,
+                        Narration = "Loan Repayment"
+                    });
+                    db.SaveChanges();
+
+                    var cr = db.LoanTransactions.Where(x => x.Loan.Id == loanId).Select(x => x.Cr).Sum();
+                    var dr = db.LoanTransactions.Where(x => x.Loan.Id == loanId).Select(x => x.Dr).Sum();
+                    if (cr <= dr)
+                    {
+                        loan.LoanStatus = "Completed";
+                        db.SaveChanges();
+                    }
+                }
+                else
+                {
+                    loan.LoanStatus = "Completed";
+                    db.SaveChanges();
+                }
+               
                 return Json(true);
             }
             return Json(false);
         }
-        [HttpPost]
-        public ActionResult Create(LoanTransaction loantransaction)
-        {
-            ViewBag.AccountNo = loantransaction.AccountNo;
 
-            var customer = db.Customers.Where(m => m.AccountNo == loantransaction.AccountNo);
-
-            if (ModelState.IsValid)
-            {
-                if (customer != null)
-                {
-                    loantransaction.DateCreated = DateTime.Now.ToShortDateString();
-                    loantransaction.Cr = 0;
-                    loantransaction.createdby = User.Identity.Name.ToString();
-
-                    db.LoanTransactions.Add(loantransaction);
-                    db.SaveChanges();
-
-                    return Json(new { Message = "Successfully made loan repayment of " + loantransaction.Dr + " for customer : "+loantransaction.AccountNo });
-                }
-                else
-                {
-                    return Json(new { Error = "Unable to make loan repayment" });
-                }
-              
-            }
-
-            return View(loantransaction);
-        }
-
+        
         //
         // GET: /LoanTransaction/Edit/5
 
